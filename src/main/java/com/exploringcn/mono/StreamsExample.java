85246e98bb25d032556f7a452967472a0e9500ec
exploringcn21 <exploringcn21@gmail.com>
2021-08-23 20:08:08 +0100

    added Stream.of example

---
+++ src/main/java/com/exploringcn/mono/StreamsExample.java
@@ -0,0 +1,18 @@
+package com.exploringcn.mono;
+
+import java.util.stream.Stream;
+
+public class StreamsExample {
+
+    // 1 - Generate stream using known elements
+    public void demoStreamOf(){
+        Stream<String> stream = Stream.of("Adam", "Ben", "Claire", "Dan");
+        stream.forEach(System.out::println);
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/test/java/com/exploringcn/mono/StreamsExampleTest.java
@@ -0,0 +1,15 @@
+package com.exploringcn.mono;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StreamsExampleTest {
+
+    StreamsExample streamsExample = new StreamsExample();
+
+    @Test
+    void demoStreamOf() {
+        streamsExample.demoStreamOf();
+    }
+}
