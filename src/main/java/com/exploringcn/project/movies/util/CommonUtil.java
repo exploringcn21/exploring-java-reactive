a1a9f44b36d1441f798de32c3d4331d75d2c9f8a
exploringcn21 <exploringcn21@gmail.com>
2021-09-07 22:20:38 +0100

    added utility class to introduce delay to movie project

---
+++ src/main/java/com/exploringcn/project/movies/util/CommonUtil.java
@@ -0,0 +1,14 @@
+package com.exploringcn.project.movies.util;
+
+import static java.lang.Thread.sleep;
+
+public class CommonUtil {
+
+    public static void delay(int ms){
+        try {
+            sleep(ms);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
